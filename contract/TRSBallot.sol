8013154501418ec61f185730ecfff57bc35df9c7
Théo Maringer <61200819+ByFishh@users.noreply.github.com>
2022-05-08 12:30:52 +0200

    Update TRSBallot.sol

--- contract/TRSBallot.sol
+++ contract/TRSBallot.sol
@@ -11,3 +11,65 @@ contract TRSBallot is Ballot { //vote classico
         
     }
 }
+
+contract TRSBallotNorm is TRSBallot {
+
+
+    constructor(string[] memory proposalNames, uint256 endDate, bool resultBeforeEnd)
+        TRSBallot(proposalNames, endDate, resultBeforeEnd)
+    {
+
+    }
+
+    function vote(string memory proposal) public {
+        require(hasAlreadyVoted(msg.sender) == false, "You have already voted in this ballot !");
+
+        for (uint i = 0; i < _proposals.length; i++) {
+            if (keccak256(bytes(_proposals[i].name)) == keccak256(bytes(proposal)))
+                _proposals[i].voteCount += 1;
+        }
+
+        _voters.push(Voter({
+            voter: msg.sender,
+            voted: true
+        }));
+    }
+}
+
+contract TRSBallotSpec is TRSBallot {
+
+    address[] _voterWhitelist;
+
+    constructor(string[] memory proposalNames, uint256 endDate, bool resultBeforeEnd, address[] memory voters)
+        TRSBallot(proposalNames, endDate, resultBeforeEnd)
+    {
+        _spec = true;
+
+        for (uint i = 0; i < voters.length; i++) {
+            _voterWhitelist.push(voters[i]);
+        }
+    }
+
+    function isInList(address addr) public view returns(bool) {
+        for (uint i = 0; i < _voterWhitelist.length; i++) {
+            if (msg.sender == addr)
+                return true;
+        }
+        return false;
+    }
+
+    function vote(string memory proposal) public {
+        require(isInList(msg.sender) == true, "Your are not allowed to participate in this ballot !");
+        require(hasAlreadyVoted(msg.sender) == false, "You have already voted in this ballot !");
+
+        for (uint i = 0; i < _proposals.length; i++) {
+            if (keccak256(bytes(_proposals[i].name)) == keccak256(bytes(proposal)))
+                _proposals[i].voteCount += 1;
+        }
+
+        _voters.push(Voter({
+            voter: msg.sender,
+            voted: true
+        }));
+    }
+}

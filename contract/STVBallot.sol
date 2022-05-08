4fb89cfbc52c146b4eeaf96a8cc25d181f10ff04
Théo Maringer <61200819+ByFishh@users.noreply.github.com>
2022-05-08 12:31:15 +0200

    Update STVBallot.sol

--- contract/STVBallot.sol
+++ contract/STVBallot.sol
@@ -11,27 +11,18 @@ contract STVBallot is Ballot { //vote by order of preference
         
     }
 }
-    //constructor(string[] memory proposalNames, address[] memory specifiedVoters, uint256 endDate, bool hasSpecifiedVoters) payable {
-    //    _hasSpecifiedVoters = hasSpecifiedVoters;
-//
-    //    for (uint i = 0; i < proposalNames.length; i++) {
-    //        _proposals[proposalNames[i]] = 0;
-    //    }
-//
 
-    //}   
-    //
-    //function vote(string[] memory proposal) public {
-    //    require(_voters[msg.sender] == false, "You already voted !");
-    //    require(block.timestamp < _endDate, "The vote is done !");
-//
-    //    for (uint i = 0; i < proposal.length; i++) {
-    //        _proposals[proposal[i]] += (proposal.length - i);
-    //    }
-    //}
-//
-    //function getProposalsPoints() public view returns (mapping(string  => uint)) {
-//
-    //}
+contract STVBallotSpec is STVBallot {
 
+    address[] _voterWhitelist;
 
+    constructor(string[] memory proposalNames, uint256 endDate, bool resultBeforeEnd, address[] memory voters)
+        STVBallot(proposalNames, endDate, resultBeforeEnd)
+    {
+        _spec = true;
+
+        for (uint i = 0; i < voters.length; i++) {
+            _voterWhitelist.push(voters[i]);
+        }
+    }
+}
